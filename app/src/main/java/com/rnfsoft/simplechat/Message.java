a8dc0a63f5a681ea0a66668a2cc0fd4ab7f47256
taehee <dadac76@hotmail.com>
2015-08-21 14:13:46 -0700

    Initial Commit

---
+++ app/src/main/java/com/rnfsoft/simplechat/Message.java
@@ -0,0 +1,26 @@
+package com.rnfsoft.simplechat;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+/**
+ * Created by taehee on 8/19/15.
+ */
+@ParseClassName("Message")
+public class Message extends ParseObject {
+    public String getUserId() {
+        return getString("userId");
+    }
+
+    public String getBody() {
+        return getString("body");
+    }
+
+    public void setUserId(String userId) {
+        put("userId", userId);
+    }
+
+    public void setBody(String body) {
+        put("body", body);
+    }
+}
